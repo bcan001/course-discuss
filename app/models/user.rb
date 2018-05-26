ac8a038b831a18770f8d782f1912cd6446eb385b
bcaneba <bcaneba@gmail.com>
2018-05-26 12:29:42 -0500

    added to validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
   validates :first_name, presence: true
 	validates :last_name, presence: true
 	validates :email, presence: true, uniqueness: true
-	validates :phone, presence: true
 	validates :username, presence: true, uniqueness: true
 
 
